56c42e2a771356c0469dd430acabb75e91866895
Nguyen Huu Loc <locn562836@gmail.com>
2024-07-25 21:32:22 +0700

    add eureka server and discovery client

---
+++ springboot-microservices/department-service/src/test/java/org/huuloc/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.huuloc.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
